7b2145037cf0d0e09232a4c6d60f5b5aa6c02626
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-05-27 11:46:32 -0500

    external.js updates

---
+++ js/external.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+console.log("Hello World");
+console.log("Hellow Polaris");
+
+alert("Hey Girl Hey");
+
+var confirmed = confirm("Do you want icecream?");
+console.log(confirmed);
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
