17bb9631c4997c4c721895fcbd32ce26d18c4467
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-05-27 14:16:31 -0500

    Finished excersies

--- js/external.js
+++ js/external.js
@@ -12,3 +12,24 @@ var favoriteColor = prompt("What is your favorite color?");
 alert("Great, " + favoriteColor + " is my favorite color too!");
 
 // Answer for 3.a
+/*
+var littleMermaid = 3;
+var brotherBear = 5;
+var hercules = 1;
+var totalCost;
+
+totalCost = ((littleMermaid * 3) + (brotherBear * 3) + (hercules * 3));
+console.log(totalCost);
+alert("The price for the movie is: " + totalCost); */
+
+// 3.b
+
+alert("You will make $" + ((400 * 6) + (380 * 4) + (350 * 10)));
+
+// 3.c
+
+var full = confirm("Is the class full?");
+var confilct = confirm("Is there a conflict?");
+alert("You can enroll in class:" + (full && !confilct));
+
+// 3.d
