94590864cf5ed6a03e1623d685cadd1e855cc054
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-05-27 13:53:25 -0500

    Excercises

--- js/external.js
+++ js/external.js
@@ -4,15 +4,11 @@
 console.log("Hello from External JavaScript");
 
 
-alert("Welcome to my website");
 
 var favColor = confirm("What is your favorite color?");
 console.log(favColor);
 
 var favoriteColor = prompt("What is your favorite color?");
-if(favoriteColor == "blue") {
-    console.log("That's my favorite color too!");
-} else {
-    console.log("That's okay!")
-}
-console.log('The user entered: ' + favoriteColor);
+alert("Great, " + favoriteColor + " is my favorite color too!");
+
+// Answer for 3.a
