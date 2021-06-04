5bafb6247611488dd0db6ceb1903af4a1fbc7f34
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-06-04 10:02:35 -0500

    Worked on stuff

--- js/loop_classwork.js
+++ js/loop_classwork.js
@@ -1,1 +1,19 @@
 //alert("Hey, Girl");
+
+var message = "";
+
+var howmany = prompt("How many?")
+
+// (initialization, condition that allows it to run, increment)
+// always 3 parts
+
+for (let index = 0; index < 10; index++) {
+    // (for (let index = 10; index > 0; index = index - 2) {
+    message = message + "index is: " + index + "\n";
+   // break; //stops this completely
+}
+// alert runs the for loop
+alert(message);
+
+alert(index);
+//alert(index) will not show up because message is global (var) and index is local (let)
