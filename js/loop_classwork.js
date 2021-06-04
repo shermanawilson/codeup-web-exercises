3293d42b56b72cb84a8416a20a9e7827d82a2c02
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-06-04 14:38:27 -0500

    Added new file, break continue excersie

--- js/loop_classwork.js
+++ js/loop_classwork.js
@@ -1,19 +1,47 @@
-//alert("Hey, Girl");
+// //alert("Hey, Girl");
+//
+// var message = "";
+//
+// var howmany = prompt("How many?")
+//
+// // (initialization, condition that allows it to run, increment)
+// // always 3 parts
+//
+// for (let index = 0; index < 10; index++) {
+//     // (for (let index = 10; index > 0; index = index - 2) {
+//     message = message + "index is: " + index + "\n";
+//    // break; //stops this completely
+// }
+// // alert runs the for loop
+// alert(message);
+//
+// alert(index);
+// //alert(index) will not show up because message is global (var) and index is local (let) */
 
-var message = "";
+// for(var index = 0; index < 5; index++) {
+//     //do something
+//     alert("index: " + index + "\n");
+// }
+//
+// var index = 0; // initialize outside of the loop
 
-var howmany = prompt("How many?")
+var notDone = true;
 
-// (initialization, condition that allows it to run, increment)
-// always 3 parts
+while (notDone) {
+    // do something
+   // alert("index: " + index + "\n");
+   // index++;
+    notDone = confirm("Do you want to go again?");
 
-for (let index = 0; index < 10; index++) {
-    // (for (let index = 10; index > 0; index = index - 2) {
-    message = message + "index is: " + index + "\n";
-   // break; //stops this completely
 }
-// alert runs the for loop
-alert(message);
+//while loops
 
-alert(index);
-//alert(index) will not show up because message is global (var) and index is local (let)
+var badData = false;
+function validateNumber() {
+    do {
+        var value = prompt("Enter a number");
+        badData = typeof(value) != "number";
+        while(!badData);
+    }
+    return value;
+}
