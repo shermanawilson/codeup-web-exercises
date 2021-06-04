3ab53726d168fd31d7354f9028e2cb412fdc4443
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-06-04 17:02:32 -0500

    Completed another assignement

--- js/while.js
+++ js/while.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// While Loops
+
 for (let index = 1; index <= 65536; index = (index * 2)) {
 
    // if(index = index * 2) {
@@ -12,19 +14,16 @@ for (let index = 1; index <= 65536; index = (index * 2)) {
 var totalCanSell = Math.floor(Math.random() * 50) + 50;
 console.log("The number of cones I can sell is: " + totalCanSell);
 
-
-var cannotSell;
 let totalSold = 0;
 do {
         var conesBought = Math.floor(Math.random() * 5) + 1;
-
-       // console.log("My customer bought" + conesSold +" cones!");
         if((totalSold + conesBought) > totalCanSell) {
                 console.log("I'm sorry, I can't sell " + conesBought);
                 continue;
         }
         console.log("My customer bought " + conesBought + "cones!")
         totalSold = totalSold + conesBought;
+
         console.log("Ive sold " + totalSold + " cones!");
 } while(totalSold < totalCanSell);
 console.log("Yay! I've sold them all!");
