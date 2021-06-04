75dcafef3d68655af7ed41a0d4f41b01adb41d89
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-06-04 16:24:30 -0500

    Completed another assignement

--- js/while.js
+++ js/while.js
@@ -1,4 +1,6 @@
 
-for (let index = 0; index <= 65536; index = index * 2) {
-    console.log(index);
+for (let index = 1; index <= 65536; index = (index * 2)) {
+
+   // if(index = index * 2) {
+        console.log(index);
 }
