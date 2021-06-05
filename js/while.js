d5507ff3f9c31cdfd1253a85c42eb2f2ad081bdf
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-06-05 13:39:20 -0500

    Went Over Do-While Loop

--- js/while.js
+++ js/while.js
@@ -12,16 +12,17 @@ for (let index = 1; index <= 65536; index = (index * 2)) {
 // Do While Loop #1
 
 var totalCanSell = Math.floor(Math.random() * 50) + 50;
-console.log("The number of cones I can sell is: " + totalCanSell);
+console.log("The total number of cones I can sell is: " + totalCanSell);
 
 let totalSold = 0;
 do {
         var conesBought = Math.floor(Math.random() * 5) + 1;
+        var notEnough = (totalCanSell - totalSold);
         if((totalSold + conesBought) > totalCanSell) {
-                console.log("I'm sorry, I can't sell " + conesBought);
+                console.log("I'm sorry, I can't sell you " + conesBought + " cones, I only have " + notEnough + " cones left");
                 continue;
         }
-        console.log("My customer bought " + conesBought + "cones!")
+        console.log("My customer bought " + conesBought + " cones!")
         totalSold = totalSold + conesBought;
 
         console.log("Ive sold " + totalSold + " cones!");
