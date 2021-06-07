4e0474ac3f8a9d4c610eab97c1ee9e7a65164f8c
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-06-07 12:25:27 -0500

    Finished Excersies

--- js/iterating.js
+++ js/iterating.js
@@ -1,37 +1,45 @@
-(function(){
+(function (){
     "use strict";
 
     /**
-     * TODO:
+     *
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+var names = ["shy", "deshawn", "armani", "aria"];
     /**
-     * TODO:
+     *
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+console.log(names.length);
     /**
-     * TODO:
+     *
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+   // console.log(names[2]);
     /**
-     * TODO:
+     *
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+for(let index= 0; index < names.length; index++) {
+    console.log(names[index]);
+}
     /**
-     * TODO:
+     *
      * Refactor your above code to use a `forEach` loop
      */
-
+names.forEach(function(name){
+   console.log("An awesome name is: " + name + ".") ;
+});
     /**
-     * TODO:
+     *
      * Create the following three functions, each will accept an array and
      * return an an element from it
      * - first: returns the first item in the array
@@ -43,5 +51,20 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    // var first = [1, 2, 3, 4, 5];
+    // var second = [1, 2, 3, 4, 5];
+    // var last = [1, 2, 3, 4, 5];
 
+    function first(array) {
+        return array[0];
+    }
+    function second(array) {
+        return array[1];
+    }
+    function last(array) {
+        return array[array.length - 1];
+    }
+    console.log(first([1, 2, 3, 4, 5]));
+    console.log(second([1, 2, 3, 4, 5]));
+    console.log(last([1, 2, 3, 4, 5]));
 })();
