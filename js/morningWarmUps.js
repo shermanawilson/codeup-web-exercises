5b68c4ba7a1156d8113b95da6893f266d92b028c
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-07-01 09:26:06 -0500

    Fixed Excersise

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -1,11 +1,19 @@
 // TODO: Create a function named countSpaces that accepts a string and returns the total number of spaces in the string.
 function countSpaces(value) {
-    const spaceCount = value.split(" ").length - 1;
-    return spaceCount;
+    if (typeof value !== "string") {
+        if (typeof value === "number") {
+            return false;
+        }
+    } else {
+        const spaceCount = value.split(" ").length - 1;
+        return spaceCount;
+    }
 }
+
 // TODO: Add validation to function above, if the argument pass is not a string it should return false.
 
-console.log(countSpaces("Hello There Everyone, My Name Is Sherma'n And I Love To Play Hippo"));
+console.log(countSpaces("Hello There Everyone, My Name Is Sherman And I Love To Play Hippo"));
 console.log(countSpaces(" "));
 console.log(countSpaces("5"));
 console.log(countSpaces("Jenny, Where did you go??"));
+console.log(countSpaces(54));
