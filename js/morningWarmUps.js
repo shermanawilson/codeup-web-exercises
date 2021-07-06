1c98adf3e8875f00d03f46642a0938b235ec7399
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-07-06 09:30:23 -0500

    Updates

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -17,3 +17,29 @@ console.log(countSpaces(" "));
 console.log(countSpaces("5"));
 console.log(countSpaces("Jenny, Where did you go??"));
 console.log(countSpaces(54));
+
+
+
+// July 6th 2021
+// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
+function capitalizeAllNames() {
+    const arr = ["fulu", "nulu", "hulu"];
+    const arrToUp = String.prototype.toUpperCase.apply(arr).split(",");
+    return arrToUp;
+}
+console.log(capitalizeAllNames());
+// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
+//  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
+
+function capitalizeFirstLetter(value) {
+    // const arraay = value;
+    // const words = arraay.split(" ");
+    // for (let i = 0; i < words.length; i++) {
+    //     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    // // return value[0].toUpperCase() + value.substring(1);
+    //     words.join(" ");
+    const finalSentence = value.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+}
+console.log(capitalizeFirstLetter(['hello world my name is shy']));
