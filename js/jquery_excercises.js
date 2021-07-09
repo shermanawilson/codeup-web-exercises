46e354934d29dea47f4d6ebf75e7c8837fd33a7e
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-07-09 11:20:49 -0500

    Updates

--- js/jquery_excercises.js
+++ js/jquery_excercises.js
@@ -1,4 +1,5 @@
 'use strict'
+// JavaScript
 
 // When you click on the text, it will say something nice :)
 // let mainOne = document.querySelector('#main-one');
@@ -12,8 +13,17 @@
 
 // J Query
 
-$(document).ready(function() {
-    const h1 = $('#main-one');
-    console.log(h1);
-    alert('You are enough');
+// $(document).ready(function() {
+//     const h1 = $('#main-one');
+//     console.log(h1);
+//     $(h1).click(alert('You are enough'));
+//
+// });
+
+
+// this calls the h1 tag with the main-one id and the .bind acts as the addEventListener
+// inside of the function has the alert. the function does not have to be named.
+$('#main-one').bind('click', function() {
+    alert("Sometimes you've got to let everything go - purge yourself. If you are unhappy with anything... " +
+        "whatever is bringing you down, get rid of it. Because you'll find that when you're free, your true creativity, your true self comes out.");
 });
