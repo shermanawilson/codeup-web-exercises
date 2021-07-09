b04eec26fb8c72b45be8937f6b442383f5513b04
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-07-09 11:54:57 -0500

    Added notes to my jquery_excercises.js file

--- js/jquery_excercises.js
+++ js/jquery_excercises.js
@@ -14,22 +14,25 @@
 // J Query
 
 // down bellow allows an alert to go off when page is loaded
+var h1;
+$(document).ready(function() {
+     h1 = $('#main-one');
+    console.log(h1);
+    h1.html('If you click the image now, I will give you a quote! :)');
+    $(h1).click(alert('You are enough'));
 
-// $(document).ready(function() {
-//     const h1 = $('#main-one');
-//     console.log(h1);
-//     $(h1).click(alert('You are enough'));
-//
-// });
+});
 
 // bellow allows the user to click on the text and have a nice quote alert to them
 
 // this calls the h1 tag with the main-one id and the .bind acts as the addEventListener
 // inside of the function has the alert. the function does not have to be named.
-$('#main-one').bind('click', function() {
+$('img').bind('click', function() {
     alert("Sometimes you've got to let everything go - purge yourself. If you are unhappy with anything... " +
         "whatever is bringing you down, get rid of it. Because you'll find that when you're free, your true creativity, your true self comes out.");
 });
 
 // *** let mainOne = document.querySelector('#main-one'); **  is equal to ** $('#main-one') **
 // mainOne.addEventListener('click', function mainFunction(); **  is equal to ** .bind('click', function() **
+
+
