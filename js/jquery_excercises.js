358cb8088666a8d0efcda688f151a8ec71c4eee1
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-07-09 10:51:56 -0500

    Updates

---
+++ js/jquery_excercises.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// long way to do this
+// let mainOne = document.querySelector('#main-one');
+// mainOne.addEventListener('click', function mainFunction() {
+//     alert('You are enough');
+// });
+
+// console.log($(document));
+
+$(document).ready(function() {
+    var h1 = $('#main-one');
+    console.log(h1);
+    alert('You are enough');
+});
