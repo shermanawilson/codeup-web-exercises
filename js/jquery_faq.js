d37ad7ccd5e2dc9478b33beb4d2253c87dec3cce
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-07-14 09:16:17 -0500

    Updates

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -13,29 +13,25 @@ $(document).ready(function () {
             $(this).css('background-color', 'yellow')
         });
     });
+
     //make the last li in each ul have a yellow background
     $('#yellowButton').click(function () {
-        $('li').last().css('background-color', '#FF0');
+        $('ul').each(function () {
+            // $('li').last().css('background-color', '#FF0'); // I needed to add . children
+            $(this).children().last().css('background-color', '#FF0');
+
+        });
     });
 
     //when any h3 is clicked, the li below should be bold
     $('h3').click(function () {
-        $('ul').children().css('font-weight', 'bold');
+        $(this).next().children().css('font-weight', 'bold');
     });
 
     // when any li is clicked, the first li of the parent ul should turn blue
     $('li').click(function () {
-        $('#state-parks1 > li').first().css('color', 'blue');
+        $(this).parent().children().first().css('color', 'blue');
     });
-
-    $('li').click(function () {
-        $('#state-parks2 > li').first().css('color', 'blue');
-    });
-
-    $('li').click(function () {
-        $('#state-parks3 > li').first().css('color', 'blue');
-    });
-
 
 
 });
